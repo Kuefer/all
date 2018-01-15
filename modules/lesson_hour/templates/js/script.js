3c7b58c8e1fd3b08f149862f1550d83f0d9fa1f9
kuefer <danrobert445@gmail.com>
2018-01-15 21:56:16 +0100

    js mask fix  for hours and remarks fix

--- modules/lesson_hour/templates/js/script.js
+++ modules/lesson_hour/templates/js/script.js
@@ -1,23 +1,19 @@
-/*global jQuery, Drupal, console */
-(function ($, Drupal) {
+/*global jQuery, Drupal */
+(function () {
 	'use strict';
-//-------------------------
-//-------------------------
-	$(document).ready(function () {
-      $('#lesson-certificate').mask("9.99");
-      $('#lesson-target').mask("9.99");
-      $('#lesson-hour').mask("99:99");
-      $('#lesson-registered').mask("99/99/9999");
-      $('#lesson-mark-date').mask("9999/99/99");
-      $('#remark-date').mask("9999/99/99");
-      $('#lesson-mark').mask("9.99");
-      $('#lesson-points').mask("9.99");
+	jQuery(document).ready(function () {
+      jQuery('#lesson-certificate').mask("9.99");
+      jQuery('#lesson-target').mask("9.99");
+      jQuery('#lesson-hour').mask("99:99");
+      jQuery('#lesson-registered').mask("99/99/9999");
+      jQuery('#lesson-mark-date').mask("9999/99/99");
+      jQuery('#remark-date').mask("9999/99/99");
+      jQuery('#lesson-mark').mask("9.99");
+      jQuery('#lesson-points').mask("9.99");
 
-        $('#new-lesson').bind('click', function() {
-          $('#submit-lesson').fadeToggle();
+        jQuery('#new-lesson').bind('click', function() {
+          jQuery('#submit-lesson').fadeToggle();
         });
 	});
-//-------------------------
-//-------------------------
 //-------------------------
 }(jQuery, Drupal));
